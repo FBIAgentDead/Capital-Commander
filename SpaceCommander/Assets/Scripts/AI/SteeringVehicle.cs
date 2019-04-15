0f69f3970ca92147a24458b61ce993d321dcbf69
Dimitri van Manen <vmanen68@gmail.com>
2019-04-15 19:24:24 +0200

    Added a space ship exterior

---
+++ SpaceCommander/Assets/Scripts/AI/SteeringVehicle.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class SteeringVehicle : MonoBehaviour
+{
+    [SerializeField] private float _maxSpeed = 20;
+
+    /** hoe zwaarder het object, hoe slechter hij kan bijsturen */
+    [SerializeField] private float _mass = 20;
+
+    /** boolean of het object de richting op kijkt waar we naar toe bewegen */
+    [SerializeField] private bool _followPath = false;
+
+    [SerializeField] private float _arrivalDistance = 0.1f;
+
+    public Vector3 _currentVelocity;
+    private Vector3 _currentPosition;
+
+    /** Vector om de locatie bij te houden waar we heen willen */
+    private Vector3 _currentTarget;
+
+    public Action Arrived;
+
+    void Start()
+    {
+        _currentVelocity = new Vector3(0, 0, 0);
+        _currentPosition = transform.position;
+    }
+
+    // Elke frametick kijken we hoe we moeten sturen
+    void Update()
+    {
+        Seek();
+    }
+
+    public void SetTarget(Vector3 target)
+    {
+        _currentTarget = target;
+    }
+
+    // van buitenaf kun je de huidige target uitlezen
+    public Vector3 Target
+    {
+        get { return _currentTarget; }
+    }
+
+    void Seek()
+    {
+        // we berekenen eerst de afstand/Vector tot de 'target'		
+        var desiredStep = _currentTarget - _currentPosition;
+
+        // als een vector ge'normalized' is .. dan houdt hij dezelfde richting
+        // maar zijn lengte/magnitude is 1
+        desiredStep.Normalize();
+
+        // vermenigvuldigen met de maximale snelheid dan
+        // wordt de lengte van deze Vector maxSpeed (aangezien 1 x maxSpeed = maxSpeed)
+        // de x en y van deze Vector wordt zo vanzelf omgerekend
+        var desiredVelocity = desiredStep * _maxSpeed;
+
+        // bereken wat de Vector moet zijn om bij te sturen om bij de desiredVelocity te komen
+        var steeringForce = desiredVelocity - _currentVelocity;
+
+        // uiteindelijk voegen we de steering force toe maar wel gedeeld door de 'mass'
+        // hierdoor gaat hij niet in een rechte lijn naar de target
+        // hoe zwaarder het object des te groter de bocht
+        _currentVelocity += steeringForce / _mass;
+
+        // Als laatste updaten we de positie door daar onze beweging (velocity) bij op te tellen
+        _currentPosition += _currentVelocity * Time.deltaTime;
+        transform.position = _currentPosition;
+        
+        if (_followPath)
+        {
+            //float angle = Mathf.Atan2(_currentVelocity.y, _currentVelocity.x) * Mathf.Rad2Deg;
+            //transform.eulerAngles = new Vector3(angle, 0, angle);
+            Quaternion lookAT = Quaternion.LookRotation(_currentVelocity);
+            transform.rotation = lookAT;
+        }
+
+        if (Vector3.Distance(_currentTarget, _currentPosition) < _arrivalDistance)
+        {
+            Arrived?.Invoke();
+        }
+    }
+}
---
+++ SpaceCommander/Assets/Scripts/AI/TargetManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetManager : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+
+    private void Start()
+    {
+        GetComponent<SteeringVehicle>().SetTarget(target.position);
+        GetComponent<SteeringVehicle>().Arrived += SetAgain;
+    }
+
+    private void SetAgain()
+    {
+        GetComponent<SteeringVehicle>().SetTarget(target.position);
+    }
+
+}
